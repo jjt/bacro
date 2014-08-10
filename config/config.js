c7e610d88e23660593cb9ece8c804b82da17d857
Jason Trill <jason@jasontrill.com>
2014-08-10 13:19:39 -0700

    Add config.js

---
+++ config/config.js
@@ -0,0 +1,117 @@
+var path = require('path')
+  , _ = require('lodash')
+  , rootPath = path.normalize(__dirname + '/..')
+  , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
+  , hostname = process.env.hostname || 'http://bacro.localdev.com'
+  , notifier = {
+      service: 'postmark',
+      APN: false,
+      email: false, // true
+      actions: ['comment'],
+      tplPath: templatePath,
+      key: 'POSTMARK_KEY',
+      parseAppId: 'PARSE_APP_ID',
+      parseApiKey: 'PARSE_MASTER_KEY'
+    }
+
+module.exports = {
+  development: {
+    db: 'mongodb://localhost/bacro_dev',
+    root: rootPath,
+    notifier: notifier,
+    app: {
+      name: 'Nodejs Express Mongoose Demo'
+    },
+    facebook: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: hostname + "/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: hostname + "/auth/twitter/callback"
+    },
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://bacro.localdev.com/auth/github/callback'
+    },
+    google: {
+      clientID: "1059096846902-cbmdbhqphmne2tv2mduha2elfpljnrii.apps.googleusercontent.com",
+      clientSecret: require('./googleSecret.LOCAL'),
+      callbackURL: hostname + "/auth/google/callback"
+    },
+    linkedin: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: hostname + "/auth/linkedin/callback"
+    }
+  },
+  test: {
+    db: 'mongodb://localhost/bacro_test',
+    root: rootPath,
+    notifier: notifier,
+    app: {
+      name: 'Nodejs Express Mongoose Demo'
+    },
+    facebook: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: hostname + "/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: hostname + "/auth/twitter/callback"
+    },
+    github: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: hostname + "/auth/github/callback"
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: hostname + "/auth/google/callback"
+    },
+    linkedin: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: hostname + "/auth/linkedin/callback"
+    }
+  },
+  production: {
+    db: process.env.MONGOLAB_URI,
+    root: rootPath,
+    notifier: notifier,
+    app: {
+      name: 'Nodejs Express Mongoose Demo'
+    },
+    facebook: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: hostname + "/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: hostname + "/auth/twitter/callback"
+    },
+    github: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: hostname + "/auth/github/callback"
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: hostname + "/auth/google/callback"
+    },
+    linkedin: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: hostname + "/auth/linkedin/callback"
+    }
+  }
+}
